f91a56d1964dd492e34ea3611655f948dd9ab25b
ZFlem96 <zackzacharyfleming@gmail.com>
2016-07-11 19:28:19 -0600

    end of 7/11/16

---
+++ WebLinkFinder/src/app/Crawler.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Crawler {
+	ArrayList<String> toVisit;
+	ArrayList<String> hasVisit;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
